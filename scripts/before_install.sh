6e438b8d2d88f2175885f9b50b3bc576ab6a1fd3
Ankit Singh Kandari (codertacina) <codertacina@gmail.com>
2022-01-14 17:53:27 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-# Install forever module 
-# https://www.npmjs.com/package/forever
-sudo npm install pm2 -g
-
-# Clean working folder
-# sudo find /home/ubuntu/test -type f -delete
+npm install pm2 -g
