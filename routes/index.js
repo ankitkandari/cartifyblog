b6fb8cfdc2f78f285aef28c76a37babf3d7f933c
Ankit Singh <codertacina@gmail.com>
2022-01-16 01:25:58 +0530

    fix

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express JS this fucking is working' });
+    res.render('index', { title: 'Express JS this fucking is working' });
+});
+router.get('/fuckluck', function(req, res, next) {
+    res.render('index', { title: 'Fuck did it' });
 });
 
 module.exports = router;
