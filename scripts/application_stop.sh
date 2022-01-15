30cc7b63017a7394662657c5594e4fa86d2bb59a
Ankit Singh Kandari (codertacina) <codertacina@gmail.com>
2022-01-15 23:45:15 +0530

    Update application_stop.sh

--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+#add npm and node to path
+export NVM_DIR="$HOME/.nvm"	
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+
+pkill node
